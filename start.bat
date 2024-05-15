a2a54de64921ed5a10a9566ff26dbaab4564daac
ilhamhacker <45305710+ilhamhacker@users.noreply.github.com>
2024-05-15 23:48:27 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,8 +14,9 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a troop tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret.Maybe your previous VM is still running:
+https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @Shiro
-echo Silahkan Login Ke RDP Anda Sekarang
+echo Password: @GetdDrive!$
+echo Please log in to your RDP!
 ping -n 10 127.0.0.1 >nul
